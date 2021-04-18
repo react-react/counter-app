bd960f8e124ab4f06582399f778d3c61fb399886
dell <huytientai@gmail.com>
2021-04-18 15:28:34 +0700

    condition for list

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -6,36 +6,26 @@ class Counter extends Component {
     tags: ["tag1", "tag2", "tag3"],
   };
 
-  myStyles = {
-    fontSize: 30,
-    fontWeight: "bold",
-  };
+  renderTags() {
+    if (this.state.tags.length === 0) {
+      return <p>There are no tags</p>;
+    }
+    return (
+      <ul>
+        {this.state.tags.map((tag, idx) => (
+          <li key={idx}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
 
   render() {
     return (
       <React.Fragment>
-        <span style={this.myStyles} className={this.getBadgeClasses()}>
-          {this.formatCounter()}
-        </span>
-        <ul>
-          {this.state.tags.map((tag, idx) => (
-            <li key={idx}>{tag}</li>
-          ))}
-        </ul>
-        <button className="btn btn-secondary btn-sm ">Increment</button>
+        {this.state.tags.length === 0 && "Please add new Tags!"}
+        {this.renderTags()}
       </React.Fragment>
     );
-  }
-
-  getBadgeClasses() {
-    let classes = "m-2 badge badge-";
-    classes += this.state.counter === 0 ? "warning" : "primary";
-    return classes;
-  }
-
-  formatCounter() {
-    const { counter } = this.state;
-    return counter === 0 ? "Zero" : counter;
   }
 }
 
